385afbc93fe641822f68b2ed9d2be0c4eedcbd65
Figamus <fig74656@yahoo.com>
2018-08-01 15:06:02 -0500

    park info module phase 1 complete

--- js/parkInfo.js
+++ js/parkInfo.js
@@ -7,6 +7,31 @@ let parkInfo = {};
 db.fetchParkInfo()
    .then((result) => {
    parkInfo = result;
+   console.log(result);
+   return parkInfo;
+})
+.then((result) => {
+    let fragment = document.createDocumentFragment();
+    let card = document.createElement("div");
+    let head = document.createElement("h3");
+    let textArea = document.createElement("p");
+    let textArea2 = document.createElement("p");
+    let hours = document.createElement("p");
+    card.className = "col-md-12 mx-auto shadow";
+    head.innerText = `${result[0].name}`;
+    textArea.innerText = `${result[0].description}`;
+    textArea2.innerText = `${result[0].location}`;
+    hours.innerText = `We are open from ${result[0].operating_hours[0].opening}AM until ${result[0].operating_hours[0].closing}PM`;
+    card.appendChild(head);
+    card.appendChild(textArea);
+    card.appendChild(textArea2);
+    card.appendChild(hours);
+    fragment.appendChild(card)
+    return fragment
+})
+.then((result) => {
+    let divHolder = document.querySelector("#parkInfo")
+    divHolder.appendChild(result)
 });
 
 module.exports = parkInfo;
