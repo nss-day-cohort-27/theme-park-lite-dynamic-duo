1df8c00d41a8178c96bc9cb01d0b6d2957f8dae1
Figamus <fig74656@yahoo.com>
2018-08-02 14:27:07 -0500

    secret added

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ let parkInfo = require('./parkInfo.js');
 let attractionTypes = require("./attractionTypes.js")
 let areaDbInfo = require('./areas.js');
 let attractions = require("./attractions.js");
+
+$("#parkInfo").click(function(){
+    let target = document.querySelector("#parkInfo");
+    let newDiv = document.createElement("div");
+    newDiv.className = "batImage";
+    newDiv.innerHTML = `<img src="https://www.thereminder.com/Opinion/notefromthepublish/climb-til-your-dream-comes-true/Batman-Robin-climbing.gif,0" alt="Batman & Robin"><p>Climbing out of the Pit of Depsair</p>`;
+    target.appendChild(newDiv);
+})
